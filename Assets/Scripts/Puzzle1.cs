6f476950234410534b0ee4cf4202adbefd285e81
lanaschimitez <lana.schimitez@ibm.com>
2020-04-07 20:34:55 -0300

    Alterações nas cenas

---
+++ Assets/Scripts/Puzzle1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Puzzle1 : MonoBehaviour
+{
+    public float speed = 5.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movimento();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Barreira")
+        transform.position = new Vector3(-7.0f, -4.0f, 0f);
+        if(other.tag == "Final")
+        {
+            Debug.Log("Ganhou");
+            SceneManager.LoadScene("Sala Principal");
+        }
+    }
+
+    public void Movimento()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime; //o jogador ira mover cada vez que prescioonar 
+        transform.Translate(horizontal, 0, 0);
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime; //o jogador ira mover cada vez que prescioonar 
+        transform.Translate(0, vertical, 0);
+    }
+   
+}
---
+++ Assets/Scripts/Puzzle2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Puzzle2 : MonoBehaviour
+{
+    public string lana = "Lana";
+    void Start()
+    {
+        var input = gameObject.GetComponent<InputField>();
+        input.onEndEdit.AddListener(SubmitName);  // This also works
+
+    }
+    private void SubmitName(string arg0)
+    {
+        string lana = "Lana";
+        Debug.Log(arg0);
+
+        Debug.Log("    Is str1 equal to str2?: {0}" + string.Equals(arg0, lana));
+    }
+}
