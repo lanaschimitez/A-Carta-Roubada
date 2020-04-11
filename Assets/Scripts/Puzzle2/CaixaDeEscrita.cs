60b61a4d2c17c97ad2f9429e9f67964640a2eb20
lanaschimitez <lana.schimitez@ibm.com>
2020-04-11 12:08:51 -0300

    Modificações do Puzzle2

---
+++ Assets/Scripts/Puzzle2/CaixaDeEscrita.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CaixaDeEscrita : MonoBehaviour
+{
+    public string lana = "Lana";
+    public char[] enigma; 
+    public char[] teste;
+    void Start()
+    {
+        enigma = new char[] { 'a', 'b' }; //colocar para pegar em uma base de dados
+        var input = gameObject.GetComponent<InputField>();
+        input.onEndEdit.AddListener(SubmitName);
+
+    }
+    private void SubmitName(string arg0)
+    {
+        string lana = "Lana"; 
+        Debug.Log(arg0);
+
+        Debug.Log("    Is str1 equal to str2?: {0}" + string.Equals(arg0, lana));
+    }
+}
---
+++ Assets/Scripts/Puzzle2/TextoEnigma.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextoEnigma : MonoBehaviour
+{
+    private Text enima;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
