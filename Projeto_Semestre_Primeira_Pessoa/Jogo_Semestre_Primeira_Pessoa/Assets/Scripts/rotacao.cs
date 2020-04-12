108c0a70fc7b80142d48d11af70430e8d43455f3
lanaschimitez <lana.schimitez@gmail.com>
2020-04-12 15:10:32 -0300

    teste

---
+++ Projeto_Semestre_Primeira_Pessoa/Jogo_Semestre_Primeira_Pessoa/Assets/Scripts/rotacao.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotacao : MonoBehaviour
+{
+    float velocidadeRotacao = 10;
+    void Update()
+    {
+        if(Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Vector2 posicaoToque = Input.GetTouch(0).deltaPosition;
+            float x = posicaoToque.x * Mathf.Deg2Rad * velocidadeRotacao;
+            //float y = posicaoToque.y * Mathf.Deg2Rad * velocidadeRotacao;
+            transform.RotateAround(Vector3.zero, Vector3.up, x);
+            //transform.RotateAround(Vector3.zero, Vector3.left, -y);
+        }
+    }
+}
