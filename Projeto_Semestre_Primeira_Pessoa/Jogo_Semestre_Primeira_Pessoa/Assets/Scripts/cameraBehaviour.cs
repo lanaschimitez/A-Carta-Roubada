fb53d9ea78a05b3c2c32408ed4ed45f26adcbc60
mablackdark <mablackdark@github.com>
2020-04-11 21:59:44 -0300

    Jogo
    Jogo

---
+++ Projeto_Semestre_Primeira_Pessoa/Jogo_Semestre_Primeira_Pessoa/Assets/Scripts/cameraBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraBehaviour : MonoBehaviour
+{
+    public Transform ObjASeguir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(ObjASeguir.position.x, ObjASeguir.position.y+1, ObjASeguir.position.z);
+    }
+}
