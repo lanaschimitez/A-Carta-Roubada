21369840e0a71764073c5e22db7c8aa30f412245
marcelodanelon <marcelodanelon11@gmail.com>
2020-04-16 19:03:12 -0300

    correcao
    correcao puzzle1

--- Assets/Scripts/Puzzle1/Puzzle1.cs
+++ Assets/Scripts/Puzzle1/Puzzle1.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Puzzle1 : MonoBehaviour
+public class puzzle1 : MonoBehaviour
 {
     public float speed = 5.0f;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,9 +18,9 @@ public class Puzzle1 : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Barreira")
-        transform.position = new Vector3(-7.0f, -4.0f, 0f);
-        if(other.tag == "Final")
+        if (other.tag == "Barreira")
+            transform.position = new Vector3(-7.0f, -4.0f, 0f);
+        if (other.tag == "Final")
         {
             Debug.Log("Ganhou");
             SceneManager.LoadScene("Sala Principal");
@@ -34,5 +34,5 @@ public class Puzzle1 : MonoBehaviour
         float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime; //o jogador ira mover cada vez que prescioonar 
         transform.Translate(0, vertical, 0);
     }
-   
+
 }
