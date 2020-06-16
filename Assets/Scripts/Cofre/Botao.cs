83fe7d1d901fc4cb449e04ad26a722551d7f158f
lanaschimitez <lana.schimitez@gmail.com>
2020-06-16 09:27:02 -0300

    Atualizacoes

---
+++ Assets/Scripts/Cofre/Botao.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botao : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float reductionRadius = 1.0f;
+    float velocidadeRotacao = 20;
+    public Vector2 posicaoToque;
+    public GameObject gameObjectTodrag; //Objeto que sera movido
+
+
+    private Vector3 GOcenter; //Centro do objeto
+    private Vector3 touchPosition; //Touch ou posição do Click
+    private Vector3 offset;//vector entre touchpoint/mouseclick para o Centro do Objeto
+    private Vector3 newGOCenter; //novo Centro do objeto
+    private RaycastHit hit; //Armazena informação que pegou o objeto
+    private bool draggingMode = false;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                gameObjectTodrag = hit.collider.gameObject; //mudar para varios objetos
+                GOcenter = gameObjectTodrag.transform.position;
+                touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                offset = touchPosition - GOcenter;
+                draggingMode = true;
+                posicaoToque = Input.GetTouch(0).deltaPosition;
+
+            }
+        }
+        if (Input.GetMouseButton(0))
+        {
+            if (draggingMode)
+            {
+                touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                newGOCenter = touchPosition - offset;
+                gameObjectTodrag.transform.rotation = Quaternion.Euler(0, 0, GOcenter.z);
+            }
+        }
+        //Quando solta o click do mouse
+        if (Input.GetMouseButtonUp(0))
+        {
+            draggingMode = false;
+        }
+        /*if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            posicaoToque = Input.GetTouch(0).deltaPosition;
+            float z = posicaoToque.x * Mathf.Deg2Rad * velocidadeRotacao;
+            float y = posicaoToque.y * Mathf.Deg2Rad * velocidadeRotacao;
+            //transform.RotateAround(Vector3.zero, Vector3.forward, z);
+            transform.rotation = Quaternion.Euler(0, 0, posicaoToque.magnitude);
+        }*/
+    }
+}
---
+++ Assets/Scripts/Cofre/Botao2.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botao2 : MonoBehaviour
+{
+    [Range(0.01f, 90.0f)] public float anguloInclinacao = 90.0f;
+    private RaycastHit hit;
+    public bool onMouseDown = false;
+    void Update()
+    {
+        if (onMouseDown)
+        {
+            var objectPos = Camera.main.WorldToScreenPoint(transform.position);
+            var dir = Input.mousePosition - objectPos;
+            float rotacZ = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            if (rotacZ > -90.0f && rotacZ < 90.0f)
+            {
+                if (rotacZ > anguloInclinacao)
+                {
+                    rotacZ = anguloInclinacao;
+                }
+                else if (rotacZ < -anguloInclinacao)
+                {
+                    rotacZ = -anguloInclinacao;
+                }
+            }
+            else
+            {
+                if (rotacZ < (180.0f - anguloInclinacao) && rotacZ > 0.0f)
+                {
+                    rotacZ = (180.0f - anguloInclinacao);
+                }
+                else if (rotacZ > -(180.0f - anguloInclinacao) && rotacZ < 0.0f)
+                {
+                    rotacZ = -(180.0f - anguloInclinacao);
+                }
+            }
+            transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotacZ));
+        }
+    }
+    private void OnMouseUp()
+    {
+        onMouseDown = false;
+    }
+
+    private void OnMouseDrag()
+    {
+        onMouseDown = true;
+    }
+
+    private void OnMouseExit()
+    {
+        onMouseDown = false;
+    }
+}
